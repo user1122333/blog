e575b64f26ce2a1858813f9b8317abe9ac5b682a
Sergioo <journalgentleman4@gmail.com>
2019-08-11 00:39:43 +0200

    aggiunta pacchetto laravelCollective per i form

--- resources/views/homepages/homepage.blade.php
+++ resources/views/homepages/homepage.blade.php
@@ -6,7 +6,13 @@
 </head>
 
 <body>
-
+    {!! Form::open(['route'=> 'homepage']) !!}
+    <div class="form-group">
+        <!--'email',nome della colonna della tabella-->
+        {!! Form::label('email' ,'Email:' , ['style'=>'h3']) !!}<br>
+        {!! Form::text('email' , '' , ['class'=>'form-control','style'=>'width:220px','placeholder'=>'Inserisci@la mail']) !!}
+    </div>
+    {{ Form::close() }}
 </body>
 
 </html>
