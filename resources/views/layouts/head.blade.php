08fdff14baa95c4b36042d17ddd1c909e7d566a8
Sergioo <journalgentleman4@gmail.com>
2019-08-12 10:12:35 +0200

    tutto

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <h1 style="text-align: center; color: red;background-color: lightgreen"> Gentleman_Journal Blog </h1>
+</head>
+
+<body>
+
+</body>
+</html>
---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html >
+<head>
+@include('layouts.head')
+</head>
+
+<body>
+
+@yield('campo_variabile');
+@include('layouts.errors');
+
+</body>
+</html>
